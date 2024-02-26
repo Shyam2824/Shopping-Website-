1901da37a213518654b44ec54255c82113e807d3
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-02-26 20:58:46 +0530

    first commit

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.css'
+
+
+function Header() {
+  return (
+
+   <div className="header">
+      <div className="headerLeft">
+        <Link style={{textDecoration:'none',color:'aliceblue'}} to="/">
+        <h2>Shopping Items</h2>
+        </Link>
+      </div>
+   </div>
+
+  )
+}
+
+export default Header
